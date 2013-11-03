a857bca10ed802069078900424f2ce2decd9d628
Romain Beaumont <romain.rom1@gmail.com>
2013-11-04 00:55:14 +0100

    fix a bug about language

--- subtitle.pm
+++ subtitle.pm
@@ -26,19 +26,21 @@ sub get_subtitle_page
 	my $page=$res->content;
 	if(!($page=~/<tr><td><img src="images\/television.png" \/><\/td><td><a href="(.+?)" debug="[0-9]+">.+?<\/a><\/td><\/tr>/)) {bug("get subtitle page");}
 	my $subtitlePage=$1;
-	return $subtitlePage;
+	return "http://www.addic7ed.com/".$subtitlePage;
 	
 }
 
 sub get_subtitle
 {
 	my ($videoFileName)=@_;
-	my $url="http://www.addic7ed.com/".get_subtitle_page($videoFileName);
+	my $url=get_subtitle_page($videoFileName);
+# 	print($url."\n");
 	my $req=HTTP::Request->new(GET=>$url);
 	my $res=$ua->request($req);
 	my $page=$res->content;
-	if(!($page=~/<td width="21%" class="language">English<a href="javascript:saveFavorite\(([0-9]+),([0-9]+),0\)">/)) {bug("get subtitle");}
-	my $subtitle="http://www.addic7ed.com/original/".$1."/".$2;
+	if(!($page=~/<td width="21%" class="language">English<a href="javascript:saveFavorite.+?">.+?<a class="buttonDownload" href="(.+?)"><strong>/s)) {bug("get subtitle");}
+	my $subtitle="http://www.addic7ed.com".$1;
+# 	print($subtitle."\n");
 	my @a=split('\.',$videoFileName);
 	pop(@a);
 	my $subtitleFileName=join('.',@a).".en.srt";
